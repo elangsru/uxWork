19405edfa2a9faacc90706cc0720f5dd285b3efe
Espen Langsrud <espen.langsrud@gmail.com>
2026-05-19 12:15:47 +0200

    Bedre icons og ios design

--- app/paymentConfirmation/page.tsx
+++ app/paymentConfirmation/page.tsx
@@ -284,54 +284,67 @@ function ReceiptSheet({ onClose }: { onClose: () => void }) {
 
 function CloseIcon() {
   return (
-    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round">
-      <path d="M6 6 L18 18 M18 6 L6 18" />
+    <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round">
+      <path d="M5 5 L19 19 M19 5 L5 19" />
     </svg>
   );
 }
 
 function CheckIconSmall() {
   return (
-    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M5 12.5 L10 17.5 L20 6.5" />
+    <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M5 12.5 L10 17.5 L19.5 7" />
     </svg>
   );
 }
 
 function ChevronRightIcon() {
   return (
-    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M9 6 L15 12 L9 18" />
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M6 3 L11 8 L6 13" />
     </svg>
   );
 }
 
 function MoneyBagIcon() {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M9 4 L15 4 L13 7 L11 7 Z" />
-      <path d="M11 7 C 6 9 4 14 6 18 C 7 20 9 21 12 21 C 15 21 17 20 18 18 C 20 14 18 9 13 7" />
-      <path d="M10.5 13 H13.5 M11.2 11 V15.5 M12.8 11 V15.5" />
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M8.25 7.5H15m-.378 2.25H8.628c-2.371 1.77-4.878 4.67-4.878 7.5 0 3.918 1.945 6 7.875 6s7.875-2.082 7.875-6c0-2.83-2.507-5.73-4.878-7.5Zm1.878-9-9 1.5s1.5.818 1.5 3h4.875c0-2.182 2.625-4.5 2.625-4.5Z"
+      />
+      <path
+        fill="currentColor"
+        d="M10.799 19.5h1.575l-1.944-2.511L12.239 15h-1.611l-1.314 1.638V13.2H8v6.3h1.314v-1.287l.324-.36 1.161 1.647Zm4.651-4.59c-.576 0-1.071.306-1.323.774V15h-1.17v4.5h1.314v-2.457c0-.657.387-.999 1.152-.999.189 0 .36.018.495.045v-1.125a1.733 1.733 0 0 0-.468-.054Z"
+      />
     </svg>
   );
 }
 
 function BubbleIcon() {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M4 5 H20 V16 H13 L8 20 V16 H4 Z" />
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M12.75.75a10.485 10.485 0 0 0-8.916 16.025L.75 23.25l6.474-3.083A10.5 10.5 0 1 0 12.75.75v0Z"
+      />
     </svg>
   );
 }
 
 function CalendarIcon() {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round">
-      <rect x="3.5" y="5" width="17" height="15" rx="2.5" />
-      <path d="M3.5 9.5 H20.5" />
-      <path d="M8 3 V6 M16 3 V6" />
-      <path d="M14 13 a3 3 0 1 0 0 4" />
-      <path d="M16 11 V14 H13" />
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+      <path
+        fill="currentColor"
+        d="M7.25 1a.75.75 0 0 0-1.5 0h1.5Zm-1.5 5a.75.75 0 0 0 1.5 0h-1.5Zm12.5-5a.75.75 0 0 0-1.5 0h1.5Zm-1.5 5a.75.75 0 0 0 1.5 0h-1.5ZM9 23.25a.75.75 0 0 0 0-1.5v1.5Zm3.333-7.226a.75.75 0 0 0 1.416.494l-1.416-.494Zm10.626.247-.709.247.073.207.172.136.464-.59Zm.708 3.705a.75.75 0 0 0-1.417-.494l1.417.494Zm-10.626-.247.708-.247-.072-.207-.172-.136-.464.59ZM23.25 16.5v.75a.75.75 0 0 0 .75-.75h-.75Zm.75-3.25a.75.75 0 0 0-1.5 0H24Zm-4 2.5a.75.75 0 0 0 0 1.5v-1.5Zm-4 4.5a.75.75 0 0 0 0-1.5v1.5Zm-3.25-.75v-.75a.75.75 0 0 0-.75.75h.75ZM12 22.75a.75.75 0 0 0 1.5 0H12ZM5.75 1v2.5h1.5V1h-1.5Zm0 2.5V6h1.5V3.5h-1.5Zm11-2.5v2.5h1.5V1h-1.5Zm0 2.5V6h1.5V3.5h-1.5ZM2.5 23.25H9v-1.5H2.5v1.5Zm19-20.5h-4v1.5h4v-1.5Zm-4 0h-11v1.5h11v-1.5Zm-11 0h-4v1.5h4v-1.5ZM.75 4.5v5h1.5v-5H.75Zm0 5v12h1.5v-12H.75Zm22.5-1v-4h-1.5v4h1.5ZM1.5 10.25h20v-1.5h-20v1.5ZM21.75 8.5a.25.25 0 0 1-.25.25v1.5a1.75 1.75 0 0 0 1.75-1.75h-1.5Zm-.25-4.25a.25.25 0 0 1 .25.25h1.5a1.75 1.75 0 0 0-1.75-1.75v1.5Zm-19 17.5a.25.25 0 0 1-.25-.25H.75c0 .966.784 1.75 1.75 1.75v-1.5Zm0-19A1.75 1.75 0 0 0 .75 4.5h1.5a.25.25 0 0 1 .25-.25v-1.5Zm11.25 13.768A4.502 4.502 0 0 1 18 13.5V12a6.002 6.002 0 0 0-5.667 4.024l1.416.494ZM18 13.5a4.502 4.502 0 0 1 4.25 3.018l1.417-.494A6.002 6.002 0 0 0 18 12v1.5Zm4.25 5.982A4.502 4.502 0 0 1 18 22.5V24a6.002 6.002 0 0 0 5.667-4.024l-1.417-.494ZM18 22.5a4.502 4.502 0 0 1-4.25-3.018l-1.417.494A6.002 6.002 0 0 0 18 24v-1.5Zm6-6v-3.25h-1.5v3.25H24Zm-.75-.75H20v1.5h3.25v-1.5Zm-7.25 3h-3.25v1.5H16v-1.5Zm-4 .75v3.25h1.5V19.5H12Zm10.495-2.64.292.23.926-1.18-.291-.229-.927 1.18Zm-8.99 2.28-.292-.23-.926 1.18.29.229.928-1.18Z"
+      />
     </svg>
   );
 }
@@ -339,8 +352,8 @@ function CalendarIcon() {
 function CameraIcon() {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M4 8 H7 L9 6 H15 L17 8 H20 V18 H4 Z" />
-      <circle cx="12" cy="13" r="3.5" />
+      <path d="M3 8 H7 L8.5 6 H15.5 L17 8 H21 V18 C 21 18.6 20.6 19 20 19 H4 C 3.4 19 3 18.6 3 18 Z" />
+      <circle cx="12" cy="13" r="3.6" />
     </svg>
   );
 }
